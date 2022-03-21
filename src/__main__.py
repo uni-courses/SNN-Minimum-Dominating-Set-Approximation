5cef16cec9e39451d4368e89b0aebd98ca5c995d
some <some@some.some>
2022-03-21 19:03:07 +0100

    Improved formatting of main file.

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,8 @@
-# This is the main code of this project nr, and it manages running the code and
-# outputting the results to latex.
-import argparse
+## Entry point for this project, runs the project code and exports data if
+# export commmands are given to the cli command that invokes this script.
 
+
+## Import used functions.
 # Project code imports.
 from .create_planar_triangle_free_graph import get_graph
 from .neumann import compute_mtds
@@ -12,16 +13,16 @@ from .arg_parser import parse_cli_args
 from .export_data.export_data import export_data
 
 
+## Parse command line interface arguments to determine what this script does.
 args = parse_cli_args()
 
-# Run main code.
+
+## Run main code.
 G = get_graph(args, False)
 # compute_mtds(G)
 compute_mtds_a_t_0(G)
 
-print(f"Done with main code.")
-# exit()
 
-# Run data export code if any argument is given.
+## Run data export code if any argument is given.
 if not all(arg is None for arg in [args.l, args.dd, args.sd, args.c2l, args.ec2l]):
     export_data(args)
