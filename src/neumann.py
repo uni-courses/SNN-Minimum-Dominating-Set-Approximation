af3d1abdcbc7695b93aa0327c68665e3c7ef11bc
some <some@some.some>
2022-03-21 13:30:33 +0100

    Included faulty max computation.

--- src/neumann.py
+++ src/neumann.py
@@ -7,32 +7,36 @@ def compute_mtds(G):
     if G.is_directed():
         raise Exception("Parameter graph should be undirected")
 
+    
+
     # 1.a Each vertex v_i chooses random float r_i in range 0<r_i<1
     for node in G.nodes:
+        #print(f'label={G.nodes[node]["label"]}')
         G.nodes[node]["rand_val"] = random.uniform(0, 1)
         print(f'rand_val={G.nodes[node]["rand_val"]}')
     # 1.b Each vertex v_i computes d_i. d_i=degree of vertex v_i
     for node in G.nodes:
-        G.nodes[node]["degree"] = G.nodes[node]["degree"] = G.degree(node)
+        G.nodes[node]["degree"] = G.degree(node)
         print(f'degree={G.nodes[node]["degree"]}')
     # 1.c Each vertex v_i computes weight: w_i=d_i+r_i
     for node in G.nodes:
         G.nodes[node]["weight"] = G.nodes[node]["degree"] + G.nodes[node]["rand_val"]
         print(f'weight={G.nodes[node]["weight"]}')
 
-    # 1.d Each vertex v_i sends w_i to each of its neighbours.max.
+    # 1.d Each vertex v_i sends w_i to each of its neighbours.
     # TODO: paradigm: Either each neuron sends some value to another neuron.
     # OR: each neuron asks/demands some value of each of its neighbours.
-    for node in G.nodes:
-        max = 0
-        for neighbor in G.neighbors(node):
-            if max < G.nodes[neighbor]["rand_val"]:
-                max = G.nodes[neighbor]["rand_val"]
+    store_index_max_neighbour_weight(G)
 
     # 2.a Each vertex v_i gets the index of the
     # neighbouring vertex v_j_(w_max) that has the heighest w_i, with i!=j.
     # 2.b Each vertex v_i adds a mark to that neighbour vertex v_j_(w_max).
-
+    for node in G.nodes:
+        G.nodes[node]["mark"]=0
+    for node in G.nodes:
+        neighbor_with_max_weight=G.nodes[node]["neighbor_with_max_weight"]
+        G.nodes[neighbor_with_max_weight]["mark"]=G.nodes[neighbor_with_max_weight]["mark"]+1
+        print(f'node={node},mark={G.nodes[neighbor_with_max_weight]["mark"]}')
     # 3 for k in range [0,m] rounds, do:
 
     # 4.a Each node v_i computes how many marks it has received, as (x_i)_k.
@@ -45,3 +49,17 @@ def compute_mtds(G):
     # 6.c Each vertex v_i gets the index of the
     # neighbouring vertex v_j_(w_max) that has the heighest w_i, with i!=j.
     # 6.d Each vertex v_i adds a mark to that neighbour vertex v_j_(w_max).
+
+def store_index_max_neighbour_weight(G):
+    for node in G.nodes:
+        G.nodes[node]["neighbor_with_max_weight"]=None
+        max=0 # Assumes positive weights
+        for neighbor in G.neighbors(node):
+            if G.nodes[node]["neighbor_with_max_weight"] is None:
+                G.nodes[node]["neighbor_with_max_weight"]=neighbor#G.nodes[neighbor]
+                max = G.nodes[neighbor]["weight"]
+            elif G.nodes[neighbor]["weight"]> max:
+                G.nodes[node]["neighbor_with_max_weight"]=neighbor#G.nodes[neighbor]
+                max = G.nodes[neighbor]["weight"]
+        print(f'node={node}G.nodes[node]["neighbor_with_max_weight"]={G.nodes[node]["neighbor_with_max_weight"]}')
+    return G
