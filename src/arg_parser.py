ac582d317fe5989ae469097c5d7b2554ee4e2168
some <some@some.some>
2022-03-21 19:50:37 +0100

    Refactored export code.

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -1,5 +1,5 @@
 # This is the main code of this project nr, and it manages running the code and
-# outputting the results to latex.
+# outputting the results to LaTex.
 import argparse
 
 
@@ -8,49 +8,53 @@ def parse_cli_args():
     parser = argparse.ArgumentParser(description="Optional app description")
 
     ## Include argument parsing for default code.
+    # Allow user to load a graph from file.
     parser.add_argument(
         "--g",
         dest="graph_from_file",
         action="store_true",
         help="boolean flag, determines whether energy analysis graph is created from file ",
     )
+
+    # Allow user to specify an infile.
     parser.add_argument("infile", nargs="?", type=argparse.FileType("r"))
 
+    # Specify default argument values for the parser.
     parser.set_defaults(
         infile=None, graph_from_file=False,
     )
 
     ## Include argument parsing for data exporting code.
-    # Compile Latex
+    # Compile LaTex
     parser.add_argument(
-        "--l", action="store_true", help="Boolean indicating if code compiles latex"
+        "--l", action="store_true", help="Boolean indicating if code compiles LaTex"
     )
 
-    # Generate, compile and export Dynamic PlantUML diagrams.
+    # Generate, compile and export Dynamic PlantUML diagrams to LaTex.
     parser.add_argument(
         "--dd",
         action="store_true",
         help="A boolean indicating if code generated diagrams are compiled and exported.",
     )
-    # Generate, compile and export Static PlantUML diagrams.
+    # Generate, compile and export Static PlantUML diagrams to LaTex.
     parser.add_argument(
         "--sd",
         action="store_true",
         help="A boolean indicating if static diagrams are compiled and exported.",
     )
 
-    # Export the project code to latex.
+    # Export the project code to LaTex.
     parser.add_argument(
         "--c2l",
         action="store_true",
-        help="A boolean indicating if project code is exported to latex.",
+        help="A boolean indicating if project code is exported to LaTex.",
     )
 
-    # Export the exporting code to latex.
+    # Export the exporting code, and the project code to LaTex.
     parser.add_argument(
         "--ec2l",
         action="store_true",
-        help="A boolean indicating if code that exports code is exported to latex.",
+        help="A boolean indicating if code that exports code is exported to LaTex.",
     )
 
     # Load the arguments that are given.
--- src/export_data/export_data.py
+++ src/export_data/export_data.py
@@ -1,5 +1,8 @@
 # Data export imports.
-from .Export_manager import Export_manager
+from .Hardcoded_data import Hardcoded_data
+
+from .create_dynamic_diagrams import create_dynamic_diagrams
+from .create_static_diagrams import create_static_diagrams
 from .latex_export_code import export_code_to_latex
 from .latex_compile import compile_latex
 from .plantuml_generate import generate_all_dynamic_diagrams
@@ -9,107 +12,28 @@ from .plantuml_to_tex import export_diagrams_to_latex
 
 def export_data(args):
 
-    # Specify hardcoded data.
-    print(f"Hi, I'll export some data, I'll let you know when I'm done.")
-    main_latex_filename = "report.tex"
-    export_data_dirname = "export_data"
-    path_to_export_data = f"src/{export_data_dirname}"
-    append_export_code_to_latex = True
-
-    # Specify code configuration details
-    comile_locally = False
-    await_compilation = True
-    verbose = True
-    gantt_extension = ".uml"
-    diagram_extension = ".png"
-
-    # Specify paths relative to root.
-    jar_path_relative_from_root = f"{path_to_export_data}/plantuml.jar"
-    path_to_dynamic_gantts = f"{path_to_export_data}/Diagrams/Dynamic"
-    path_to_static_gantts = f"{path_to_export_data}/Diagrams/Static"
-    dynamic_diagram_output_dir_relative_to_root = f"latex/Images/Diagrams"
-
-    ## Run main code.
-    export_manager = Export_manager()
+    hd = Hardcoded_data()
 
     ## PlantUML
-    # Generate PlantUML diagrams dynamically (using code).
-    if args.dd:
-        generate_all_dynamic_diagrams(f"{path_to_export_data}/Diagrams/Dynamic")
-
-        # Compile dynamically generated PlantUML diagrams to images.
-        compile_diagrams_in_dir_relative_to_root(
-            await_compilation,
-            gantt_extension,
-            jar_path_relative_from_root,
-            path_to_dynamic_gantts,
-            verbose,
-        )
-
-        # Export dynamic PlantUML text files to LaTex.
-        export_diagrams_to_latex(
-            path_to_dynamic_gantts,
-            gantt_extension,
-            dynamic_diagram_output_dir_relative_to_root,
-        )
-
-        # Export dynamic PlantUML diagram images to LaTex.
-        export_diagrams_to_latex(
-            path_to_dynamic_gantts,
-            diagram_extension,
-            dynamic_diagram_output_dir_relative_to_root,
-        )
-
-    if args.sd:
-        # Compile statically generated PlantUML diagrams to images.
-        compile_diagrams_in_dir_relative_to_root(
-            await_compilation,
-            gantt_extension,
-            jar_path_relative_from_root,
-            path_to_static_gantts,
-            verbose,
-        )
-
-        # Export static PlantUML text files to LaTex.
-        export_diagrams_to_latex(
-            path_to_static_gantts,
-            gantt_extension,
-            dynamic_diagram_output_dir_relative_to_root,
-        )
-
-        # Export static PlantUML diagram images to LaTex.
-        export_diagrams_to_latex(
-            path_to_static_gantts,
-            diagram_extension,
-            dynamic_diagram_output_dir_relative_to_root,
-        )
+    create_dynamic_diagrams(args, hd)
+    create_static_diagrams(args, hd)
 
     ## Plotting
     # Generate plots.
-
     # Export plots to LaTex.
-
-    import os
-
-    print(os.getcwd())
 
     ## Export code to LaTex.
     if args.c2l:
         # TODO: verify whether the latex/{project_name}/Appendices folder exists before exporting.
         # TODO: verify whether the latex/{project_name}/Images folder exists before exporting.
-        export_code_to_latex(main_latex_filename, False)
+        export_code_to_latex(hd.main_latex_filename, False)
     elif args.ec2l:
         # TODO: verify whether the latex/{project_name}/Appendices folder exists before exporting.
         # TODO: verify whether the latex/{project_name}/Images folder exists before exporting.
-        export_code_to_latex(main_latex_filename, append_export_code_to_latex)
-
-    print(f"RUNNING COMPILE")
-    import os
-
-    print(os.getcwd())
+        export_code_to_latex(hd.main_latex_filename, hd.append_export_code_to_latex)
 
     ## Compile the accompanying LaTex report.
     if args.l:
         compile_latex(True, True)
         print("")
-    print(f"\n\nDone.")
+    print(f"\n\nDone exporting data.")
