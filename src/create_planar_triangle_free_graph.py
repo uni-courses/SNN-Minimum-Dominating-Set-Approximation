6763b55e573cee0dc68aa1427d29d9be2775395c
a-t-0 <34750068+a-t-0@users.noreply.github.com>
2022-04-27 18:09:26 +0200

    Reduced verbosity. (#64)
    Co-authored-by: some <some@some.some>

--- src/create_planar_triangle_free_graph.py
+++ src/create_planar_triangle_free_graph.py
@@ -40,7 +40,7 @@ def create_manual_graph_with_4_nodes():
             (0, 1),
             (0, 2),
             (0, 3),
-            (1, 3),
+            (1, 2),
             (2, 3),
         ]
     )
