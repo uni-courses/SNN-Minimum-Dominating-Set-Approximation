9266d5962c6f74baa6170a62960052f87790c1d7
name <some@email.com>
2022-04-30 00:01:05 +0200

    Selected smaller graphs.

--- test/test_get_results_minimal.py
+++ test/test_get_results_minimal.py
@@ -56,10 +56,10 @@ class Test_counter(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(Test_counter, self).__init__(*args, **kwargs)
 
-    def get_graphs_for_this_test(self, size=None, random=None):
+    def get_graphs_for_this_test(self, size=None, seed=None):
         # Get list of planer triangle free graphs.
-        if not size is None and not random is None:
-            G = create_triangle_free_planar_graph(size, 0.6, 42, False)
+        if not size is None and not seed is None:
+            G = create_triangle_free_planar_graph(size, 0.6, seed, False)
             print("TRIANGLE")
         else:
             G = create_manual_graph_with_4_nodes()
@@ -73,32 +73,31 @@ class Test_counter(unittest.TestCase):
         # delete_dir_if_exists(f"latex/Images/graphs")
         delete_files_in_folder(f"latex/Images/graphs")
 
-        for m in range(1, 2):
+        for m in range(0, 1):
             plot_neuron_behaviour = True
             for retry in range(0, 1, 1):
-                for size in range(5, 6, 1):
-                    G = self.get_graphs_for_this_test(size=None, random=None)
+                for size in range(3, 4, 1):
+                    G = self.get_graphs_for_this_test(size=size, seed=42)
 
                     latest_time = print_time("Create object.", datetime.now())
                     # Initialise paramers used for testing.
                     test_object = create_test_object(G, retry, m, False, False)
                     # sim_time=test_object.inhibition + 1
-                    sim_time = 2
+                    sim_time = 4
                     latest_time = print_time("Created object.", latest_time)
 
-                    # Implement brain adaptation
-                    ###implement_adaptation_mechanism(
-                    ###    G, test_object.get_degree, m, retry, size, test_object
-                    ###)
-                    # latest_time = print_time("Get adapted networkx Graph.", latest_time)
+                    # Implement brain adaptation on networkx graph.
+                    implement_adaptation_mechanism(
+                        G, test_object.get_degree, m, retry, size, test_object
+                    )
+                    latest_time = print_time("Get adapted networkx Graph.", latest_time)
 
-                    # Create monitors to probe if neuron spiked or not.
-                    ##test_object = convert_new_graph_to_snn(test_object)
-                    # latest_time = print_time("Got adapted SNN.", latest_time)
-                    # raise Exception("DONE")
+                    # Convert the graph with brain adaptation to an SNN.
+                    test_object = convert_new_graph_to_snn(test_object)
+                    latest_time = print_time("Got adapted SNN.", latest_time)
+                    
                     # monitors = create_neuron_monitors(test_object, test_object.sim_time)
                     # latest_time = print_time("Got neuron monitors.", latest_time)
-                    # raise Exception("STOP")
 
                     # Run default tests on neurons and get counted degree from
                     # neurons after inhibition time.
