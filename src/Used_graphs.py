981042e7491e9bd02c43ae4cbf1735e6abfd3a2d
name <some@email.com>
2022-04-30 23:01:23 +0200

    Included graphs used for testing.

---
+++ src/Used_graphs.py
@@ -0,0 +1,235 @@
+import networkx as nx
+
+
+class Used_graphs:
+    """Creates graphs used for paper."""
+
+    def __init__(self):
+        self.three = None
+        self.four = None
+        self.Five = None
+
+    def get_graphs_with_3_neurons(self):
+        return [self.three_a()]
+
+    def get_graphs_with_4_neurons(self):
+        return [self.four_a(), self.four_b(), self.four_c()]
+
+    def get_graphs_with_4_neurons(self):
+        return [
+            self.five_a(),
+            self.five_b(),
+            self.five_c(),
+            self.five_d(),
+            self.five_d(),
+        ]
+
+    def three_a(self):
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+            ]
+        )
+        return graph
+
+    def four_a(self):
+        """Straigt line"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+                (2, 3),
+            ]
+        )
+        return graph
+
+    def four_b(self):
+        """Y"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 2),
+                (1, 2),
+                (2, 3),
+            ]
+        )
+        return graph
+
+    def four_c(self):
+        """Square"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 0),
+            ]
+        )
+        return graph
+
+    def five_a(self):
+        """Straigt line"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3, 4],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+            ]
+        )
+        return graph
+
+    def five_b(self):
+        """Y-long-tail"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3, 4],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 2),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+            ]
+        )
+        return graph
+
+    def five_c(self):
+        """Y with 3 arms"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3, 4],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 2),
+                (1, 2),
+                (3, 2),
+                (4, 2),
+            ]
+        )
+        return graph
+
+    def five_d(self):
+        """Pentagon"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3, 4],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 0),
+            ]
+        )
+        return graph
+
+    def five_d(self):
+        """Square-with-tail"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3, 4],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 0),
+                (3, 4),
+            ]
+        )
+        return graph
+
+    def five_e(self):
+        """Square"""
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            [0, 1, 2, 3],
+            color="w",
+        )
+        graph.add_edges_from(
+            [
+                (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 0),
+            ]
+        )
+        return graph
+
+
+class Run:
+    def __init__(
+        self,
+        died_neurons,
+        G,
+        get_degree,
+        has_adaptation,
+        m,
+        pass_fail,
+        rand_values,
+        selected_alipour_nodes,
+        sim_time,
+        t,
+    ):
+        """Called at end of run."""
+        self.pass_fail = pass_fail
+        self.selected_alipour_nodes = selected_alipour_nodes
+        self.rand_values = rand_values
+        self.m = m
+        self.t = t
+        self.G = G
+        self.has_adaptation = has_adaptation
+        if self.has_adaptation:
+            self.get_degree_without_adaptation = get_degree
+        else:
+            self.get_degree_with_adaptation = get_degree
+        self.died_neurons = died_neurons
+        self.sim_time = sim_time
+
+        self.amount_extra_neurons = self.get_amount_of_neurons()
+        self.amount_extra_synapses = self.get_amount_synapses()
+        self.nr_of_spikes = self.get_nr_of_spikes()
+
+    def get_amount_of_neurons(self, get_degree):
+        return None
+
+    def get_amount_synapses(self, get_degree):
+        return None
+
+    def get_nr_of_spikes(self, get_degree):
+        return None
